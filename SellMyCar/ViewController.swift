de78924dceb92a1d03ac53f116f6670dbb73d31b
Mert <mert.ejd@gmail.com>
2021-04-25 20:32:10 +0300

    Initial Commit

---
+++ SellMyCar/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SellMyCar
+//
+//  Created by Mert Ejder 2  on 25.04.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
